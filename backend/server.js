6ee2a9d9821f8bc311842c77ce2c07490e4ea445
rendiero25 <workspace.rendy@gmail.com>
2026-02-01 02:03:31 +0700

    fixed vercel connection

--- backend/server.js
+++ backend/server.js
@@ -72,6 +72,10 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
